02acce680756a8c6baf85a8ec04fba838d852721
Anderson Lopes <developes.anderson@gmail.com>
2024-07-24 08:29:28 -0400

    🚀 Projeto Finalizado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import * as S from './styles'
+
+import cesta from '../../assets/cesta.png'
+import { paraReal } from '../Produto'
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+
+const Header = () => {
+  const itens = useSelector(
+    (state: RootReducer) => state.carrinho.itensNoCarrinho
+  )
+
+  const valorTotal = itens.reduce((acc, item) => {
+    acc += item.preco
+    return acc
+  }, 0)
+
+  return (
+    <S.Header>
+      <h1>EBAC Games</h1>
+      <div>
+        <img src={cesta} alt="" />
+        <span>
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
+        </span>
+      </div>
+    </S.Header>
+  )
+}
+
+export default Header
